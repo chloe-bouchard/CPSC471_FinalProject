6a54e695bb086f6617e0ed1d3701bb921778f535
chloe-bouchard <65249093+chloe-bouchard@users.noreply.github.com>
2022-03-23 23:45:36 -0600

    Update Donor.py

--- Donor.py
+++ Donor.py
@@ -1,10 +1,8 @@
 from Database import mysql
 from App import app
-#Author: Gibran Akmal
 
 #initialize class
 class Donor:
-    #set engg type data member (String)
     def __init__(self, FirstName, LastName, Birthdate, Gender, Password, Email, HealthNumber, Weight):
         self.account = Account(FirstName, LastName, Birthdate, Gender, Password, Email)
         self.HealthNumber = HealthNumber
