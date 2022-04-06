482f8a10102ba432b59e615193d8cea9a60bb69e
aashiki <aashik.ilangovan@ucalgary.ca>
2022-04-06 16:38:34 -0600

    Delete account.sql

--- MySQLTables/account.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM erythrodb.account
-LIMIT 0, 1000
-
--- Date: 2022-04-06 16:21
-*/
-INSERT INTO `` (`idaccount`,`First_Name`,`Last_Name`,`BirthDate`,`Gender`,`Password`,`Age`,`Email_Address`) VALUES (1,'Aashik','Ilangovan','April 10 2001','Male','Password',20,'aashik@gmail.com');
-INSERT INTO `` (`idaccount`,`First_Name`,`Last_Name`,`BirthDate`,`Gender`,`Password`,`Age`,`Email_Address`) VALUES (2,'John','Cena','June 25th 1988','Male','JohnCena',45,'johncena@yahoo.com');
