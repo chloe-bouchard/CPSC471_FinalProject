054c1a5022ec683f1c8bbf7f53ce00dd033f3d6c
chloe-bouchard <65249093+chloe-bouchard@users.noreply.github.com>
2022-04-09 10:43:16 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,40 +2,33 @@ from flask import Flask, redirect, url_for, render_template, request, jsonify, f
 
 from App import app
 from Database import mysql
-
+from Login import *
+import mysql.connector
 HOST = "http://TheErythroSite.pythonanywhere.com"
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
 from flask_mail import Mail
-# from flask_debugtoolbar import DebugToolbarExtension
 
-
-
-db = SQLAlchemy()
-DB_NAME = "database.db"
-
+def make_connection():
+    connection = mysql.connector.connect(host = '127.0.0.1', user = 'root', passwd = 'yourpassword', database = 'erythrodb')
+    return connection
 
 def create_app():
     app = Flask(__name__)
-#     app.config.from_object(os.environ['APP_SETTINGS'])
-    app.config['SECRET_KEY'] = 'stranger_things_17'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SECURITY_PASSWORD_SALT'] = 'un_deux_trois'
-    db.init_app(app)
-
-    login_manager = LoginManager()
-    login_manager.init_app(app)
     mail = Mail(app)
 
     app.config['BCRYPT_LOG_ROUNDS'] = 13
     app.config['WTF_CSRF_ENABLED'] = True
     app.config['DEBUG_TB_ENABLED'] = False
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    app.config['SECRET_KEY'] = 'super secret key'
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
 
+    sess.init_app(app)
+    app.config['SESSION_TYPE'] = 'filesystem'
+    # app.config['SECURITY_PASSWORD_SALT'] = 'un_deux_trois'
 
         # mail settings
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -50,58 +43,73 @@ def create_app():
         # mail accounts
     app.config['MAIL_DEFAULT_SENDER'] = 'TheErythroSite@gmail.com'
 
-    from .views import views
-    from .auth import auth
-
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
-
-    from .models import User, Note, Dashboard
-
-    create_database(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
     return app
 
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
 
-
-
-
-
-
-
-
-
-
-
-
-username = None
-professionFilter = None
-
+app.config['SECRET_KEY'] = 'super secret key'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    
     return render_template("main.html")
+
+@app.route('/administrator', methods=['GET', 'POST'])
+def administrator():
+    
+    return render_template("addAppointment.html")
 
 @app.route('/sign-up', methods=['GET', 'POST'])
 def signUp():
+    if request.method == "POST":
+        newConnection = make_connection() #create connection to database
+
+        #retrieve all the user inputs
+        name = request.form.get("name")
+        lastname = request.form.get("lastname")
+        email = request.form.get("email")
+        password1 = request.form.get("pass1")
+        password2 = request.form.get("pass2")
+        birthday = request.form.get("date")
+        gender = request.form.get("gender")
+        accountType = request.form.get("accountType")
+        age = 13
+        #end of input retrieval
+
+        myLogin = Login(email, password1, newConnection) #create Login object
+        if(password1 == password2): #check if both password inputs match
+            if(myLogin.add_login(name, lastname, age, birthday, gender, accountType) == True): #add account into database
+                flash('Account was successfully created!')
+                if(accountType == "Administrator"): #if account type is administator
+                    return redirect(url_for('administrator', user = myLogin)) #show administrator view
+                else:
+                    return redirect(url_for('loggedIn', user = myLogin)) #show donor view
+            else:
+                flash('Account using entered email already exists',category= 'error')
+        else:
+            return render_template("signup.html")
+
+
     return render_template("signup.html")
 
 @app.route('/sign-in', methods=['GET', 'POST'])
 def signIn():
+    if request.method == "POST":
+        newConnection = make_connection() #create connection to the database
+        #retrieve user inputs
+        name = request.form.get("email") 
+        password = request.form.get("psw")
+
+        myLogin = Login(name, password, newConnection) #create Login object
+        if(myLogin.authenticate() == 1): #if password and username exist and match
+            flash('Logged in successfully!')
+            if(myLogin.isAdmin() == "0"):
+                return redirect(url_for('loggedIn', user = myLogin)) #show donor view
+            else:
+                return redirect(url_for('administrator', user = myLogin)) #show administrator view
+        else:
+            flash('Email or password are invalid',category= 'error')
+
     return render_template("signin.html")
 
 @app.route('/logged-in', methods=['GET', 'POST'])
